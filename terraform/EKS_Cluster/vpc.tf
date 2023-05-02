1104f9ca4cf00dca7d511a7c187930359ffb6141
hvaandres <hvaandres@icloud.com>
2023-05-01 21:39:52 -0600

    EKS changes

--- terraform/EKS_Cluster/vpc.tf
+++ terraform/EKS_Cluster/vpc.tf
@@ -1,26 +1,28 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
-
-  name = "vpc"
-
-  cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = 1
-  }
+variable "region" {
+    default = "us-east-1"
+}
+data "aws_availability_zones" "available" {}
+locals {
+    cluster_name = "Workshop-EKS-Cluster"
+}
+module vpc {
+    source = "terraform-aws-modules/vpc/aws"
+    version = "3.2.0"
+    name = "workshop-VPC"
+    cidr = "10.0.0.0/16"
+    azs = data.aws_availability_zones.available.names
+    private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    public_subnets =  ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    enable_nat_gateway = true
+    single_nat_gateway = true
+    enable_dns_hostname = true
+tags = {
+    "Name" = "workshop-VPC"
+}
+public_subnet_tags = {
+    "Name" = "workshop-Public-Subnet"
+}
+private_subnet_tags = {
+    "Name" = "workshop-Private-Subnet"
+}
 }
